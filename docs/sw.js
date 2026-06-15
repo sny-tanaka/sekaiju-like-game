35316322bdab88723ad7aa72d265469ac474f397
tanashun <36751082+sny-tanaka@users.noreply.github.com>
2026-06-15 22:47:03 +0900

    階段アイコンを専用画像に差し替え（上り/下り） (#25)

--- docs/sw.js
+++ docs/sw.js
@@ -1,27 +1,27 @@
 if (!self.define) {
   let e,
     i = {};
-  const n = (n, s) => (
-    (n = new URL(n + '.js', s).href),
-    i[n] ||
+  const s = (s, n) => (
+    (s = new URL(s + '.js', n).href),
+    i[s] ||
       new Promise((i) => {
         if ('document' in self) {
           const e = document.createElement('script');
-          ((e.src = n), (e.onload = i), document.head.appendChild(e));
-        } else ((e = n), importScripts(n), i());
+          ((e.src = s), (e.onload = i), document.head.appendChild(e));
+        } else ((e = s), importScripts(s), i());
       }).then(() => {
-        let e = i[n];
-        if (!e) throw new Error(`Module ${n} didn’t register its module`);
+        let e = i[s];
+        if (!e) throw new Error(`Module ${s} didn’t register its module`);
         return e;
       })
   );
-  self.define = (s, c) => {
+  self.define = (n, r) => {
     const o = e || ('document' in self ? document.currentScript.src : '') || location.href;
     if (i[o]) return;
-    let r = {};
-    const t = (e) => n(e, o),
-      l = { module: { uri: o }, exports: r, require: t };
-    i[o] = Promise.all(s.map((e) => l[e] || t(e))).then((e) => (c(...e), r));
+    let c = {};
+    const t = (e) => s(e, o),
+      l = { module: { uri: o }, exports: c, require: t };
+    i[o] = Promise.all(n.map((e) => l[e] || t(e))).then((e) => (r(...e), c));
   };
 }
 define(['./workbox-1ef09536'], function (e) {
@@ -31,12 +31,14 @@ define(['./workbox-1ef09536'], function (e) {
   }),
     e.precacheAndRoute(
       [
-        { url: 'index.html', revision: '4c7b10d42116c3bc82349de3e958003a' },
+        { url: 'index.html', revision: '280eb62fb103fea060bf509744f0544e' },
         { url: 'icon-512.png', revision: 'aea6735f0e3902695c58dbca460dc603' },
         { url: 'icon-192.png', revision: '2abc40b6b11f19c114b43abb7733a687' },
         { url: 'favicon.ico', revision: 'bf53cfc1c1a7432fa532b35b76682d50' },
+        { url: 'assets/stairs-up-DhyZlujG.png', revision: null },
+        { url: 'assets/stairs-down-BjaF19rU.png', revision: null },
         { url: 'assets/index-DsW4N4Z-.css', revision: null },
-        { url: 'assets/index-CJeeWtlG.js', revision: null },
+        { url: 'assets/index-DHHpcfSa.js', revision: null },
         { url: 'favicon.ico', revision: 'bf53cfc1c1a7432fa532b35b76682d50' },
         { url: 'icon-192.png', revision: '2abc40b6b11f19c114b43abb7733a687' },
         { url: 'icon-512.png', revision: 'aea6735f0e3902695c58dbca460dc603' },
