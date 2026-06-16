efece131051dd6ee0850eae7a851d906d498d001
tanashun <36751082+sny-tanaka@users.noreply.github.com>
2026-06-16 22:28:24 +0900

    fix(bgm): iOS Safari で BGM 用 AudioContext をジェスチャー内で解錠して鳴らす (#39) (#46)

--- docs/sw.js
+++ docs/sw.js
@@ -19,9 +19,9 @@ if (!self.define) {
     const o = e || ('document' in self ? document.currentScript.src : '') || location.href;
     if (i[o]) return;
     let c = {};
-    const l = (e) => s(e, o),
-      t = { module: { uri: o }, exports: c, require: l };
-    i[o] = Promise.all(n.map((e) => t[e] || l(e))).then((e) => (r(...e), c));
+    const t = (e) => s(e, o),
+      l = { module: { uri: o }, exports: c, require: t };
+    i[o] = Promise.all(n.map((e) => l[e] || t(e))).then((e) => (r(...e), c));
   };
 }
 define(['./workbox-1ef09536'], function (e) {
@@ -31,14 +31,14 @@ define(['./workbox-1ef09536'], function (e) {
   }),
     e.precacheAndRoute(
       [
-        { url: 'index.html', revision: 'fe56914eee585bbb3e8ecefb5b7cb6d2' },
+        { url: 'index.html', revision: '73af47e981911e6165f049e00d90f96e' },
         { url: 'icon-512.png', revision: 'aea6735f0e3902695c58dbca460dc603' },
         { url: 'icon-192.png', revision: '2abc40b6b11f19c114b43abb7733a687' },
         { url: 'favicon.ico', revision: 'bf53cfc1c1a7432fa532b35b76682d50' },
         { url: 'assets/stairs-up-DhyZlujG.png', revision: null },
         { url: 'assets/stairs-down-BjaF19rU.png', revision: null },
+        { url: 'assets/index-ITHtDyQk.js', revision: null },
         { url: 'assets/index-CsxAsJu3.css', revision: null },
-        { url: 'assets/index-Bli2gcYF.js', revision: null },
         { url: 'favicon.ico', revision: 'bf53cfc1c1a7432fa532b35b76682d50' },
         { url: 'icon-192.png', revision: '2abc40b6b11f19c114b43abb7733a687' },
         { url: 'icon-512.png', revision: 'aea6735f0e3902695c58dbca460dc603' },
