4fa808018c6e89cbf3860ff712eba7411abcd17e
tanashun <36751082+sny-tanaka@users.noreply.github.com>
2026-06-21 21:23:15 +0900

    v1.0.12: セーブの引き継ぎを clipboard 方式からファイル方式に切り替え (#94)

--- docs/sw.js
+++ docs/sw.js
@@ -31,7 +31,7 @@ define(['./workbox-1ef09536'], function (s) {
   }),
     s.precacheAndRoute(
       [
-        { url: 'index.html', revision: '90a23d97381e4d389fcc60b344ca66ab' },
+        { url: 'index.html', revision: '0b552fbcf1762f03e678083211959dd4' },
         { url: 'icon-512.png', revision: '5316052a37fc817af9f7cd34b7f0d35d' },
         { url: 'icon-192.png', revision: '60fb1f9ababc08708199d504d57fffb0' },
         { url: 'favicon.ico', revision: '2e16a14e5099649dda716dd699ef613b' },
@@ -103,8 +103,8 @@ define(['./workbox-1ef09536'], function (s) {
         { url: 'assets/item_mat_t0_soft_pelt-B45_Umk6.png', revision: null },
         { url: 'assets/item_mat_t0_faint_ember-mc7xjJJ1.png', revision: null },
         { url: 'assets/item_bat_wing-DSYSFTv5.png', revision: null },
-        { url: 'assets/index-COxuLuBZ.js', revision: null },
-        { url: 'assets/index-Bn5S01s4.css', revision: null },
+        { url: 'assets/index-DZ9z9tzQ.js', revision: null },
+        { url: 'assets/index-CnjfLj82.css', revision: null },
         { url: 'assets/equip_t2_light-CoHmd7sL.png', revision: null },
         { url: 'assets/equip_cloth_robe-DNIpUHZ8.png', revision: null },
         { url: 'favicon.ico', revision: '2e16a14e5099649dda716dd699ef613b' },
